6e3c7e7d2b02b33df68332b5fc643c84713293e6
Hassan ur Rahim Khan <19102027.hassanurrahimkhan@gmail.com>
2021-05-01 11:48:33 +0500

    first Assignment

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
             //CHAP 1 Tasks
 //Task 1 
-//alert("Welcome to my website")
+alert("Welcome to my website")
 
 //task2
-//alert("Error! Please enter a Valid password ")
+alert("Error! Please enter a Valid password ")
 
 //task3
-//alert("Welcome to js Land "+"\n" +"Happy Coding!")
+alert("Welcome to js Land "+"\n" +"Happy Coding!")
 
 //task4
-// alert("welcome to JS Land...")
-// alert("Happy Coding!" + "\n" +" " + "prevent this page to create additional dialogs")
+alert("welcome to JS Land...")
+alert("Happy Coding!" + "\n" +" " + "prevent this page to create additional dialogs")
 
 //task5
-//console.log("hello... i can run JS through my web browser's console")
+console.log("hello... i can run JS through my web browser's console")
 
 
 
@@ -53,7 +53,7 @@ var email = "19102027.hassanurrahimkhan@gmail.com"
 var myemail = "MY email is"
 alert(myemail+" "+email)
 
-///task-7
+//task-7
 
 var book = "A smarter way to learn JavaScript"
 var abca = "I am trying to learn from the Book"
@@ -70,3 +70,46 @@ document.write("Yah! I can write HTML content through javascript")
 
 var StringDesign = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
 alert(StringDesign)
+
+                        //chap 3 Taks
+
+
+//task 1
+var age = 21
+alert("My age is: " + age)
+
+//task 2
+
+var visited = 12
+alert("You visited "+visited+" times")
+
+//task-3
+
+var birthYear = 2000
+document.write("My birth year is " + birthYear+"<br>")
+
+document.write("Datatype of my decleared variable is " + typeof(birthYear))
+
+////task-4
+var name = prompt("Enter your name:")
+
+
+var type = prompt("which type of item you want?")
+
+
+var qty = +prompt("Enter quatity")
+
+var storeName = "denim"
+
+
+document.write(name+" " + "ordered " + qty +" " +type+" " + storeName)
+
+
+                ////CHAP-5 tASKS 
+
+var num1 = +prompt("Enter First Number :")
+var num2 = +prompt("Enter Second Number")
+
+var sum = num1+num2
+document.write("Sum of "+num1+ "and "+ num2+ " is" + sum)
+
