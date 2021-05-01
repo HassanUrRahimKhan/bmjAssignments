1b5a3b20e7e00ceffe22893668b3ddd21dea797f
Hassan ur Rahim Khan <19102027.hassanurrahimkhan@gmail.com>
2021-05-01 11:07:07 +0500

    first Assignment

--- app.js
+++ app.js
@@ -14,3 +14,59 @@
 
 //task5
 //console.log("hello... i can run JS through my web browser's console")
+
+
+
+            //CHAP 2 TASKS
+
+//task-1 
+var username;
+
+//task-2
+var myName = "Hassan ur Rahim Khan"
+
+//task-3
+var titledMessage 
+titledMessage = "Hello-World"
+alert(titledMessage)
+
+
+//task-4
+
+var name = "Hassan"
+var age = 21
+var education = "bscs"
+
+alert("Name is:"  + name)
+alert("study: "  + education)
+alert("Age is: " + age)
+
+
+//task-5
+
+var abc = "pizza" + "\n"+ "pizz" +"\n"+ "piz" +"\n"+"pi"+"\n"+"p"
+alert(abc)
+
+///task-6
+
+var email = "19102027.hassanurrahimkhan@gmail.com"
+var myemail = "MY email is"
+alert(myemail+" "+email)
+
+///task-7
+
+var book = "A smarter way to learn JavaScript"
+var abca = "I am trying to learn from the Book"
+
+alert(abca + " "+book)
+
+
+
+//task-8
+
+document.write("Yah! I can write HTML content through javascript")
+
+//task-9
+
+var StringDesign = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(StringDesign)
