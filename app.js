7b1a6507bf31b0d1728f307246e643d650d18c4c
Hassan ur Rahim Khan <19102027.hassanurrahimkhan@gmail.com>
2021-05-01 05:35:36 +0500

    first Assignment

--- app.js
+++ app.js
@@ -9,3 +9,8 @@
 //alert("Welcome to js Land "+"\n" +"Happy Coding!")
 
 //task4
+// alert("welcome to JS Land...")
+// alert("Happy Coding!" + "\n" +" " + "prevent this page to create additional dialogs")
+
+//task5
+//console.log("hello... i can run JS through my web browser's console")
