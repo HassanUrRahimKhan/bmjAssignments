1952f379f57d1a9ba3619588068029ccbaf929bb
Hassan ur Rahim Khan <19102027.hassanurrahimkhan@gmail.com>
2021-06-28 00:27:44 +0500

    calculator

--- arrays.js
+++ arrays.js
@@ -1,132 +1,180 @@
-                                //Chap-14 to 16
+//                                 //Chap-14 to 16
 
-                                //TASK-1
-var studentsNames = []
-                                //TASK-2
+//                                 //TASK-1
+// var studentsNames = []
+//                                 //TASK-2
 
-var fruits = ["mango","banana","apple","apricot","pineapple"]
-                                //TASK-3
+// var fruits = ["mango","banana","apple","apricot","pineapple"]
+//                                 //TASK-3
 
-var numbers = [11,22,33,44,55,66,77,88,99]
+// var numbers = [11,22,33,44,55,66,77,88,99]
 
-                                //TASK-4
+//                                 //TASK-4
 
-var bol = [false,false,false,true,true]
+// var bol = [false,false,false,true,true]
 
-document.write(bol)
-                                //TASK-5
-
-
-var mixedArray = ["hassan",21,"bscs",true,24.5]
-
-                                //TASK-6
-
-var Qualifications = new Array("ssc","hsc","bcom","bs","MS","M-Phil","PHD")
-
-for(var i=0;i<Qualifications.length;i++)
-{
-    document.write(Qualifications[i],"<br>")
-
-}
-
-                                //TASK-6
-
-var names = ["Hassan","Anas","Ayesha"]
-var scores = [492,400,384]
-var tot = 500
-for(var j = 0;j<names.length;j++)
-{
-    document.write("score of "+ names[j]+" is "+scores[j]+" . "+" percentage" + (scores[j]/500)*100+"<br>")
-}
-                                //TASK-7
+// document.write(bol)
+//                                 //TASK-5
 
 
- var colors = ["White","blue","green","yellow","black"]
+// var mixedArray = ["hassan",21,"bscs",true,24.5]
 
- document.write(colors,"<br>")
+//                                 //TASK-6
 
- var newColor = prompt("What color do you want to add to the start array")
+// var Qualifications = new Array("ssc","hsc","bcom","bs","MS","M-Phil","PHD")
 
- colors.unshift(newColor)
+// for(var i=0;i<Qualifications.length;i++)
+// {
+//     document.write(Qualifications[i],"<br>")
 
- document.write("New array after insertion at the start " + colors +"<br>")
+// }
 
+//                                 //TASK-6
 
-
- var end =  prompt("What color do you want to add to the end array")
- colors.push(end)
- document.write("new array after insertion at the end is " + colors +"<br>")
-
- colors.push("Pink")
- colors.push("Grey")
-
- document.write("Array after deletion o first element " +"<br>")
-
- colors.shift()
- document.write(colors)
-
- var pouch = +prompt("At which position you want to add the color")
- var pouch1 = prompt("color you want to add")
- if(pouch>colors.length)
-    console.log("out of range")
-else
-{
-    colors[pouch]=pouch1
-    document.write("<br>","new array is" +colors)
-}
+// var names = ["Hassan","Anas","Ayesha"]
+// var scores = [492,400,384]
+// var tot = 500
+// for(var j = 0;j<names.length;j++)
+// {
+//     document.write("score of "+ names[j]+" is "+scores[j]+" . "+" percentage" + (scores[j]/500)*100+"<br>")
+// }
+//                                 //TASK-7
 
 
-var user = +prompt("At which index you want to delete colors")
-var user1 = +prompt("How many elements you want to delete")
+//  var colors = ["White","blue","green","yellow","black"]
 
-if(user<colors.length()||user>colors.length())
-    console.log("out of range")
-else
-{
-    colors.splice(user,user1)
-    document.write("After deletion " + colors)
-}
-colors.splice(user,user1)
-document.write("After deletion " + colors)
+//  document.write(colors,"<br>")
 
-                            //TASK-7
+//  var newColor = prompt("What color do you want to add to the start array")
 
-        var arr = [7,4,8,6,9,3,4,6,1]
-        arr.sort()
-        document.write(arr)      
+//  colors.unshift(newColor)
 
-                        //TASK-8
+//  document.write("New array after insertion at the start " + colors +"<br>")
 
 
-var stringArray = ["karachi","seoni","balakot","naran","lahore","islamabad"]
 
-var selectedCities = stringArray.slice(1,5)
-document.write("Before Selection" +" <br>"+ stringArray+"<br>")
-document.write("After Selection"+"<br>"+selectedCities)
+//  var end =  prompt("What color do you want to add to the end array")
+//  colors.push(end)
+//  document.write("new array after insertion at the end is " + colors +"<br>")
 
-                        //TASK-9
+//  colors.push("Pink")
+//  colors.push("Grey")
 
-var cat = ["This","is","my","cat"]
+//  document.write("Array after deletion o first element " +"<br>")
 
-var con = cat[0]+" "+cat[1]+" "+cat[2]+" "+cat[3]
-document.write(con)
+//  colors.shift()
+//  document.write(colors)
 
-                        //TASK-10
+//  var pouch = +prompt("At which position you want to add the color")
+//  var pouch1 = prompt("color you want to add")
+//  if(pouch>colors.length)
+//     console.log("out of range")
+// else
+// {
+//     colors[pouch]=pouch1
+//     document.write("<br>","new array is" +colors)
+// }
 
-var acces = ["keyboard","mouse","printer","inkjet","cpu"]
-document.write("Array before fifo"+acces+"<br>")
 
-for(var k=0;k<5;k++)//.length method is not working here
-{
-    document.write(acces.shift()+"<br>")
-}
+// var user = +prompt("At which index you want to delete colors")
+// var user1 = +prompt("How many elements you want to delete")
+
+// if(user<colors.length()||user>colors.length())
+//     console.log("out of range")
+// else
+// {
+//     colors.splice(user,user1)
+//     document.write("After deletion " + colors)
+// }
+// colors.splice(user,user1)
+// document.write("After deletion " + colors)
+
+//                             //TASK-7
+
+//         var arr = [7,4,8,6,9,3,4,6,1]
+//         arr.sort()
+//         document.write(arr)      
+
+//                         //TASK-8
 
 
-                        //TASk-11
+// var stringArray = ["karachi","seoni","balakot","naran","lahore","islamabad"]
 
-var access = ["helmet","gloves","mobile","water bottle"]
-document.write("Array before lifo"+access+"<br>")
-for(var i=0;i<4;i++)
-{
-    document.write(access.pop())
-}
+// var selectedCities = stringArray.slice(1,5)
+// document.write("Before Selection" +" <br>"+ stringArray+"<br>")
+// document.write("After Selection"+"<br>"+selectedCities)
+
+//                         //TASK-9
+
+// var cat = ["This","is","my","cat"]
+
+// var con = cat[0]+" "+cat[1]+" "+cat[2]+" "+cat[3]
+// document.write(con)
+
+//                         //TASK-10
+
+// var acces = ["keyboard","mouse","printer","inkjet","cpu"]
+// document.write("Array before fifo"+acces+"<br>")
+
+// for(var k=0;k<5;k++)//.length method is not working here
+// {
+//     document.write(acces.shift()+"<br>")
+// }
+
+
+//                         //TASk-11
+
+// var access = ["helmet","gloves","mobile","water bottle"]
+// document.write("Array before lifo"+access+"<br>")
+// for(var i=0;i<4;i++)
+// {
+//     document.write(access.pop())
+// }
+
+                        //TASK-12
+// var c=0
+// for(var i=1;i<=100;i++)
+// {
+//    document.write(i)
+//    if(c==9)
+//    {
+//        document.write("<br>")
+//        c=0
+//    }
+//    c++
+// }
+
+
+
+// var str1 = prompt("Enter your text")
+
+// for(var i=0;i<str1.length;i++)
+// {
+//     if(str1.slice(i,i+2)=="  "){
+//     alert("no double spaces")
+//     break}
+// }
+
+
+////RAndom number
+
+// var ran = (Math.random())*100+1
+
+// document.write(Math.ceil(ran))
+////DAte
+
+// var dob = new Date (prompt("Enter you date of Birth","feb 7, 2000"))
+// var dobMili = dob.getTime()
+// var today = new Date()
+// var todayMili = today.getTime()
+// var diff = todayMili=dobMili
+// var accurate = Math.floor(diff)
+// document.write(accurate)
+
+
+// function getName()
+// {
+//    var name = document.getElementById("names")
+//    alert(name.value)
+// }
+
---
+++ assignment17-20.js
@@ -0,0 +1,119 @@
+    ////initializing empty multi dimension arrys
+// var array = [[],[],[]]
+
+    ////task-2
+
+// var array1= [[0,1,2,3],[1,0,1,2],[2,1,0,1]]
+
+// document.write(array1[0],"<br>" +array1[1] +"<br>" + array[2] +"<br>")
+
+
+    ////task-3
+
+    // for(var i=1;i<=10;i++)
+    // {
+    //     document.write(i+"<br>")
+    // }
+
+
+    ////task-4
+
+
+    // var input = +prompt("Enter the number for table ", "Enter number here")
+
+    // var length = +prompt("Enter the length of an integer: ")
+
+    // for(var j=1;j<=length;j++)
+    // {
+    //     document.write(input+" " +"x" + j + " ="+j*input + "<br>")
+    // }
+
+
+    ////task-5
+
+    // var fruits = ["apple","banana","mango","orange","strawberry"]
+
+    // for(var k =0;k<fruits.length;k++)
+    // {
+    //     document.write("Element at index" +k+"is" + fruits[k] + "<br>")
+    // }
+
+
+    //// task-6
+
+    // document.write("Counting".fontsize(10)+"<br>")
+
+    // for(var l=1;l<=15;l++)
+    // {
+    //     document.write(l + " ")
+    // }
+
+    // document.write("<br>")
+
+    // document.write("Reverse Counting".fontsize(10)+"<br>")
+
+    // for(var i=10;i>=1;i--)
+    // {
+    //     document.write(i+" ")
+    // }
+
+    // document.write("<br>")
+
+    
+    // document.write("Even".fontsize(10)+"<br>")
+
+    // for(var y=0 ;y<=20;y++ )
+    // {
+    //     if(y%2==0)
+    //     {
+    //         document.write(y+" ")
+    //     }
+    // }
+
+    // document.write("<br>")
+
+    
+    // document.write("odd".fontsize(10)+"<br>")
+
+    // for(k=0;k<=20;k++)
+    // {
+    //     if(k%2!=0)
+    //     {
+    //         document.write(k+" ")
+    //     }
+    // }
+
+    // document.write("<br>")
+
+    
+    // document.write("Series".fontsize(10)+"<br>")
+
+    // for(var h =2;h<=20;h=h+2)
+    // {
+    //     document.write(h+"k"+" ")
+    // }
+
+
+    ////task-7
+
+    // var A = ["cake","applePie", "cookie","chips","patties"]
+
+    // var input1 = prompt("Enter your product" + "Enter here")
+   
+
+    // for(var arr=0;arr<A.length;arr++)
+    // {
+    //     if(input1===A[arr])
+    //     {
+    //         document.write("Item Found")
+    //         break;
+    //     }
+    // }
+
+
+    expand()
+    {
+        var n =document.getElementById("para").innerHTML
+        var paragraph = getElementById("text")
+        paragraph.innerHTML=n
+    }
